4343bc1e42d6e7c501a2be12d8d82c2f5ac3a70c
Robin Schroer <sulami@peerwire.org>
2019-10-16 15:43:28 +0200

    Add a parser

---
+++ src/nebula/parser.clj
@@ -0,0 +1,56 @@
+(ns nebula.parser)
+
+(defn- float-kind? [token-text]
+  (boolean (re-find #"^-?[0-9]*\.[0-9]+$" token-text)))
+
+(defn- integer-kind? [token-text]
+  (boolean (re-find #"^-?[0-9]+$" token-text)))
+
+(defn- keyword-kind? [token-text]
+  (boolean (re-find #"^:[a-zA-Z-_]+$" token-text)))
+
+(defn- symbol-kind? [token-text]
+  (boolean (re-find #"^[a-zA-Z-_<>!*?]+$" token-text)))
+
+(defn- parse-token [token]
+  (let [text (:text token)]
+    (assoc token :kind
+           (cond
+             (= "(" text)
+             :open-parenthesis
+             (= ")" text)
+             :close-parenthesis
+
+             (= "[" text)
+             :open-bracket
+             (= "]" text)
+             :close-bracket
+
+             (= "{" text)
+             :open-brace
+             (= "}" text)
+             :close-brace
+
+             (or (= "true" text)
+                 (= "false" text))
+             :boolean
+
+             (float-kind? text)
+             :float
+
+             (integer-kind? text)
+             :integer
+
+             (keyword-kind? text)
+             :keyword
+
+             (symbol-kind? text)
+             :symbol
+
+             :else
+             :error))))
+
+(defn parse
+  "Attaches the `:kind` to `tokens`."
+  [tokens]
+  (map parse-token tokens))

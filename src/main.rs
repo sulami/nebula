fa8db60e351b94829ceb3f8124c4899923612d75
Robin Schroer <sulami@peerwire.org>
2019-11-06 22:50:46 +0100

    Connect up the LLVM JIT to the REPL for integers
    Also start wrapping the LLVM state into a struct. There's _a lot_ to
    be done on this. The REPL doesn't work properly, types are currently
    fixed in place, and only integer atoms are supported.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-extern crate nom;
-extern crate nom_locate;
-
 use std::fmt::{Debug, Display, Formatter, Result};
-// use std::io::BufRead;
+use std::io::BufRead;
+
+// ----------------------------------------------------------------------------
+// Parsing
+// ----------------------------------------------------------------------------
 
 use nom::{
     IResult,
@@ -180,44 +181,108 @@ fn parse_token(s: Span) -> IResult<Span, Token> {
     ))(s)
 }
 
-fn parse_source(source: &str) {
-    println!("Parsing:\n\n{}\n", source);
+fn parse_source(source: &str) -> Vec<Token> {
+    // println!("Parsing:\n\n{}\n", source);
     let input = Span::new(source.as_bytes());
-    let parsed = all_consuming(many0(parse_token))(input);
-    match parsed {
-        Ok((_, tokens)) => for token in tokens {
-            println!("{:?}", token)
-        }
-        Err(e) => println!("Failed parsing {:?}", e)
+    let tokens: IResult<Span, Vec<Token>> = all_consuming(many0(parse_token))(input);
+    match tokens {
+        Ok((_, ts)) => ts,
+        Err(_) => vec!(),
     }
 }
 
+// ----------------------------------------------------------------------------
+// Compiler
+// ----------------------------------------------------------------------------
+
 use inkwell::OptimizationLevel;
 use inkwell::context::Context;
-// use inkwell::builder::Builder;
-// use inkwell::module::Module;
-use inkwell::targets::{Target, TargetMachine, RelocMode, CodeModel, FileType, InitializationConfig};
-use std::path::Path;
+use inkwell::builder::Builder;
+use inkwell::module::Module;
+// use inkwell::targets::{Target, TargetMachine, RelocMode, CodeModel, FileType, InitializationConfig};
+use inkwell::execution_engine::ExecutionEngine;
+use inkwell::values::{BasicValueEnum, FunctionValue, GenericValue};
+use inkwell::types::BasicTypeEnum;
+// use std::path::Path;
+
+struct Compiler {
+    context: Context,
+    module: Module,
+    builder: Builder,
+    jit: ExecutionEngine,
+}
+
+fn init_compiler() -> Compiler {
+    let ctx = Context::create();
+    let module = ctx.create_module("nebula");
+    let builder = ctx.create_builder();
+    let jit = module.create_jit_execution_engine(OptimizationLevel::None).unwrap();
+    Compiler {
+        context: ctx,
+        module: module,
+        builder: builder,
+        jit: jit,
+    }
+}
+
+impl Compiler {
+    // TODO needs to add a new module on every interaction
+    // might need to carry over previous scope into the new module as well
+    // see this loop:
+    // https://github.com/TheDan64/inkwell/blob/master/examples/kaleidoscope/main.rs#L1244
+
+    fn add_function(&self, name: &str, args: &[BasicTypeEnum], is_var_args: bool) -> FunctionValue {
+        // TODO variable return type
+        let fn_type = self.context.i32_type().fn_type(args, is_var_args);
+        self.module.add_function(name, fn_type, None)
+    }
+
+    fn add_constant(&self, value: u64) -> BasicValueEnum {
+        //TODO variable type
+        BasicValueEnum::IntValue(self.context.i32_type().const_int(value, false))
+    }
+
+    fn add_return(&self, value: BasicValueEnum) {
+        match value {
+            BasicValueEnum::IntValue(v) => self.builder.build_return(Some(&v)),
+            _ => panic!("only ints"),
+        };
+    }
+
+    fn call_function(&self, name: &str, _args: &[&GenericValue]) -> i32 {
+        // TODO arguments
+        // TODO variable types
+        // TODO handle lookup errors
+        let maybe_fn = unsafe {
+            self.jit.get_function::<unsafe extern "C" fn() -> i32>(name)
+        };
+        let fun = match maybe_fn {
+            Ok(f) => f,
+            Err(_) => panic!("no function"),
+        };
+        unsafe {
+            fun.call()
+        }
+    }
+}
+
+// ----------------------------------------------------------------------------
+// REPL
+// ----------------------------------------------------------------------------
+
+use std::io::Write;
+
+macro_rules! print_flush {
+    ( $( $x:expr ),* ) => {
+        print!( $($x, )* );
+
+        std::io::stdout().flush().expect("Could not flush to standard output.");
+    };
+}
 
 fn main() {
-    parse_source("(foo 3432 (dag of dags))\n ; blubber\nbar baz :kw");
-
-    let context = Context::create();
-    let module = context.create_module("toast");
-    let builder = context.create_builder();
-    let main_fn_type = context.i32_type().fn_type(&[], false); // arguments & is_var_args
-    let main_fn_val = module.add_function("main", main_fn_type, None);
-    let entry = main_fn_val.append_basic_block("entry");
-    let number = context.i32_type().const_int(4, false);
-    builder.position_at_end(&entry);
-    builder.build_return(Some(&number));
-
-    // jit compile & execution
-    let jee = module.create_jit_execution_engine(OptimizationLevel::None).unwrap();
-    unsafe {
-        let rv = jee.run_function_as_main(&main_fn_val, &[]);
-        println!("{}", rv);
-    }
+    // parse_source("(foo 3432 (dag of dags))\n ; blubber\nbar baz :kw");
+    let compiler = init_compiler();
 
     // write llvm bitcode to a file
     // might be useful for caching I guess, if we ever have performance problems
@@ -226,33 +291,54 @@ fn main() {
 
     // write a native object file
     // can be picked up with llc/gcc
-    Target::initialize_x86(&InitializationConfig::default());
-    let path = Path::new("toast.o");
-    let target = Target::from_name("x86-64").expect("failed to create target");
-    let target_machine = target.create_target_machine(
-        &TargetMachine::get_default_triple().to_string(),
-        "generic",
-        "",
-        OptimizationLevel::None,
-        RelocMode::Default,
-        CodeModel::Default,
-    ).expect("failed to create target machine");
-    let result = target_machine.write_to_file(
-        &module,
-        FileType::Object,
-        &path,
-    );
-    match result {
-        Ok(_) => {},
-        Err(_) => println!("failed to write"),
-    }
-
-    // for line in std::io::stdin().lock().lines() {
-    //     let l = line.unwrap();
-    //     if l.is_empty() {
-    //         std::process::exit(0);
-    //     } else {
-    //         parse_source(&String::from(l));
-    //     }
+    // Target::initialize_x86(&InitializationConfig::default());
+    // let path = Path::new("toast.o");
+    // let target = Target::from_name("x86-64").expect("failed to create target");
+    // let target_machine = target.create_target_machine(
+    //     &TargetMachine::get_default_triple().to_string(),
+    //     "generic",
+    //     "",
+    //     OptimizationLevel::None,
+    //     RelocMode::Default,
+    //     CodeModel::Default,
+    // ).expect("failed to create target machine");
+    // let result = target_machine.write_to_file(
+    //     &compiler.module,
+    //     FileType::Object,
+    //     &path,
+    // );
+    // match result {
+    //     Ok(_) => {},
+    //     Err(_) => println!("failed to write"),
     // }
+
+    // TODO convert to loop
+    print_flush!("> ");
+    for line in std::io::stdin().lock().lines() {
+        let l = line.unwrap();
+        if l.is_empty() {
+            std::process::exit(0);
+        } else {
+            for token in parse_source(&String::from(l)) {
+                println!("parsed: {:#?}", token);
+                match token {
+                    Token::Atom{kind: TokenKind::Integer, content, ..} => {
+                        let fn_val = compiler.add_function("repl", &[], false);
+                        let entry = fn_val.append_basic_block("entry");
+                        let n = compiler.add_constant(
+                            std::str::from_utf8(content)
+                                .unwrap()
+                                .parse::<u64>()
+                                .unwrap());
+                        // XXX why do we need this?
+                        compiler.builder.position_at_end(&entry);
+                        compiler.add_return(n);
+                        println!("{:?}", compiler.call_function("repl", &[]));
+                    },
+                    _ => panic!("don't know what to do")
+                }
+            }
+            print_flush!("> ");
+        }
+    }
 }
